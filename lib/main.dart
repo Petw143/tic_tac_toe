7eeb30415c9258b0ee7dcbcf52e3c691164fec50
Pedro Barbosa <barbosaph1805@gmail.com>
2025-02-02 00:45:20 -0300

    Desenvolvendo o método que verifica o vencedor, no caso de não haver vencedor, ele indica empate ou retorna uma String vazia

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main()
 
 class JogoDaVelhaApp extends StatelessWidget
 {
+  @override
   Widget build(BuildContext context)
   {
     return MaterialApp(
@@ -21,11 +22,89 @@ class JogoDaVelhaApp extends StatelessWidget
 }
 
 class _TelaJogoDaVelha extends StatefulWidget{
+	@override
 	_TelaJogoDaVelhaState createState() => _TelaJogoDaVelhaState();
 }
 
 class _TelaJogoDaVelhaState extends State<_TelaJogoDaVelha>
 {
+	//Criando o tabuleiro
+	List<List<String>> tabuleiro = List.generate(3, (_) => List.filled(3, ''));
+
+	// Jogador atual
+	String jogadorAtual = 'X';
+
+	// Verifica se o jogo acabou
+	bool jogoAcabou = false;
+
+	// Reinicia o tabuleiro
+	void reiniciarJogo()
+	{
+		setState(() {
+			tabuleiro = List.generate(3, (_) => List.filled(3, ''));
+			jogadorAtual = 'X';
+			jogoAcabou = false;
+		});
+	}
+
+	// Verifica o vencedor  
+	String verificarVencedor()
+	{
+		// Verifica as linhas horizontais
+		for(int i = 0; i < 3; i++)
+		{
+			if(tabuleiro[i][0] != '' &&
+			tabuleiro[i][0] == tabuleiro[i][1] &&
+			tabuleiro[i][1] == tabuleiro[i][2])
+			{
+				return tabuleiro[i][0];
+			}
+		}
+
+		//Verifica as linhas verticais
+		for(int i = 0; i < 3; i++)
+		{
+			if(tabuleiro[0][i] != '' &&
+			tabuleiro[1][i] == tabuleiro[1][i] &&
+			tabuleiro[1][i] == tabuleiro[2][i])
+			{
+				return tabuleiro[0][i];
+			}
+		}
+
+		//Verifica as diagonais
+		if(tabuleiro[0][0] != '' &&
+		tabuleiro[0][0] == tabuleiro[1][1] &&
+		tabuleiro[1][1] == tabuleiro[2][2])
+		{
+			return tabuleiro[0][0];
+		}
+		if(tabuleiro[0][2] != '' &&
+		tabuleiro[0][2] == tabuleiro [1][1] && 
+		tabuleiro[1][1] == tabuleiro [2][0])
+		{
+			return tabuleiro[0][2];
+		}
+
+		//Verifica empate
+		bool empate = true;
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				if(tabuleiro[i][j] == '')
+				{
+					empate = false;
+					break;
+				}	
+			}
+		}
+		if(empate) return 'Empate';
+
+		return '';
+
+	}
+
 	@override
 	Widget build(BuildContext context)
 	{
