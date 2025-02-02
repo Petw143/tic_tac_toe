fef57cd281f88bbeb0e7e6626ca261e0f99895b8
Pedro Barbosa <barbosaph1805@gmail.com>
2025-02-02 00:55:33 -0300

    Desenvolvimento do método jogar que é chamado toda vez que o jogador toca em um espaço do tabuleiro.

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,109 @@
-
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(JogoDaVelhaApp());
 }
 
-class JogoDaVelhaApp extends StatelessWidget
-{
+class JogoDaVelhaApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Jogo da Velha',
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: _TelaJogoDaVelha(),
     );
   }
 }
 
-class _TelaJogoDaVelha extends StatefulWidget{
-	@override
-	_TelaJogoDaVelhaState createState() => _TelaJogoDaVelhaState();
+class _TelaJogoDaVelha extends StatefulWidget {
+  @override
+  _TelaJogoDaVelhaState createState() => _TelaJogoDaVelhaState();
 }
 
-class _TelaJogoDaVelhaState extends State<_TelaJogoDaVelha>
-{
-	//Criando o tabuleiro
-	List<List<String>> tabuleiro = List.generate(3, (_) => List.filled(3, ''));
+class _TelaJogoDaVelhaState extends State<_TelaJogoDaVelha> {
+  //Criando o tabuleiro
+  List<List<String>> tabuleiro = List.generate(3, (_) => List.filled(3, ''));
 
-	// Jogador atual
-	String jogadorAtual = 'X';
+  // Jogador atual
+  String jogadorAtual = 'X';
 
-	// Verifica se o jogo acabou
-	bool jogoAcabou = false;
+  // Verifica se o jogo acabou
+  bool jogoAcabou = false;
 
-	// Reinicia o tabuleiro
-	void reiniciarJogo()
-	{
-		setState(() {
-			tabuleiro = List.generate(3, (_) => List.filled(3, ''));
-			jogadorAtual = 'X';
-			jogoAcabou = false;
-		});
-	}
+  // Reinicia o tabuleiro
+  void reiniciarJogo() {
+    setState(() {
+      tabuleiro = List.generate(3, (_) => List.filled(3, ''));
+      jogadorAtual = 'X';
+      jogoAcabou = false;
+    });
+  }
 
-	// Verifica o vencedor  
-	String verificarVencedor()
-	{
-		// Verifica as linhas horizontais
-		for(int i = 0; i < 3; i++)
-		{
-			if(tabuleiro[i][0] != '' &&
-			tabuleiro[i][0] == tabuleiro[i][1] &&
-			tabuleiro[i][1] == tabuleiro[i][2])
-			{
-				return tabuleiro[i][0];
-			}
+  // Verifica o vencedor
+  String verificarVencedor() {
+    // Verifica as linhas horizontais
+    for (int i = 0; i < 3; i++) {
+      if (tabuleiro[i][0] != '' &&
+          tabuleiro[i][0] == tabuleiro[i][1] &&
+          tabuleiro[i][1] == tabuleiro[i][2]) {
+        return tabuleiro[i][0];
+      }
+    }
+
+    //Verifica as linhas verticais
+    for (int i = 0; i < 3; i++) {
+      if (tabuleiro[0][i] != '' &&
+          tabuleiro[1][i] == tabuleiro[1][i] &&
+          tabuleiro[1][i] == tabuleiro[2][i]) {
+        return tabuleiro[0][i];
+      }
+    }
+
+    //Verifica as diagonais
+    if (tabuleiro[0][0] != '' &&
+        tabuleiro[0][0] == tabuleiro[1][1] &&
+        tabuleiro[1][1] == tabuleiro[2][2]) {
+      return tabuleiro[0][0];
+    }
+    if (tabuleiro[0][2] != '' &&
+        tabuleiro[0][2] == tabuleiro[1][1] &&
+        tabuleiro[1][1] == tabuleiro[2][0]) {
+      return tabuleiro[0][2];
+    }
+
+    //Verifica empate
+    bool empate = true;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (tabuleiro[i][j] == '') {
+          empate = false;
+          break;
+        }
+      }
+    }
+    if (empate) return 'Empate';
+
+    return '';
+  }
+
+  //Função para jogar
+  void jogar(int linha, int coluna) {
+    if (tabuleiro[linha][coluna] == '' && !jogoAcabou) {
+      setState(() {
+        tabuleiro[linha][coluna] = jogadorAtual;
+        String vencedor = verificarVencedor();
+        if (vencedor != '') {
+          jogoAcabou = true;
+          //mostrarResultado(vencedor);
+        }else{
+			jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
 		}
+      });
+    }
+  }
 
-		//Verifica as linhas verticais
-		for(int i = 0; i < 3; i++)
-		{
-			if(tabuleiro[0][i] != '' &&
-			tabuleiro[1][i] == tabuleiro[1][i] &&
-			tabuleiro[1][i] == tabuleiro[2][i])
-			{
-				return tabuleiro[0][i];
-			}
-		}
-
-		//Verifica as diagonais
-		if(tabuleiro[0][0] != '' &&
-		tabuleiro[0][0] == tabuleiro[1][1] &&
-		tabuleiro[1][1] == tabuleiro[2][2])
-		{
-			return tabuleiro[0][0];
-		}
-		if(tabuleiro[0][2] != '' &&
-		tabuleiro[0][2] == tabuleiro [1][1] && 
-		tabuleiro[1][1] == tabuleiro [2][0])
-		{
-			return tabuleiro[0][2];
-		}
-
-		//Verifica empate
-		bool empate = true;
-		for(int i = 0; i < 3; i++)
-		{
-			for(int j = 0; j < 3; j++)
-			{
-				if(tabuleiro[i][j] == '')
-				{
-					empate = false;
-					break;
-				}	
-			}
-		}
-		if(empate) return 'Empate';
-
-		return '';
-
-	}
-
-	@override
-	Widget build(BuildContext context)
-	{
-		return Scaffold(
-
-		);
-	}
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
